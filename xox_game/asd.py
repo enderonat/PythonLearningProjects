db553fab5f3746ecf5bf5f1d83e23184a04e5fe6
enderonat <83728904+enderonat@users.noreply.github.com>
2022-02-22 17:43:58 +0300

    Delete asd.py

--- xox_game/asd.py
+++
@@ -1,9 +0,0 @@
-// DEFINE YOUR FUNCTION BELOW:
-function returnDay(number) {
-    const daysW = [Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday];
-    let day = number - 1;
-    if (number > 7 || number < 1) {
-        return null;
-    }
-    else return daysW[day];
-}
